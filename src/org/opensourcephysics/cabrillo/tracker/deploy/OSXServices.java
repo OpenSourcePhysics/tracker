a373671c1d19ad2cba6e7b4cca08390dd31b4357
Doug Brown <dobrown@cabrillo.edu>
2016-07-18 18:13:29 -0700

    Improve OSXServices--files now open with double-click on OSX

--- src/org/opensourcephysics/cabrillo/tracker/deploy/OSXServices.java
+++ src/org/opensourcephysics/cabrillo/tracker/deploy/OSXServices.java
@@ -1,7 +1,9 @@
 package org.opensourcephysics.cabrillo.tracker.deploy;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
 
 import org.opensourcephysics.cabrillo.tracker.TFrame;
 import org.opensourcephysics.cabrillo.tracker.Tracker;
@@ -16,37 +18,57 @@ public class OSXServices implements com.apple.eawt.AboutHandler,
 	com.apple.eawt.QuitHandler, com.apple.eawt.PreferencesHandler, com.apple.eawt.OpenFilesHandler {
 
 	Tracker tracker;
-	ArrayList<File> filesToOpen = new ArrayList<File>();
 	
 	/**
-	 * Constructor used by TrackerStarter
+	 * Constructor used by TrackerStarter, which uses the DockIcon & OpenFileHandler
 	 */
 	public OSXServices() {
 		com.apple.eawt.Application application = com.apple.eawt.Application.getApplication();
 		application.setOpenFileHandler(this);
+		ImageIcon icon = new ImageIcon(
+	      Tracker.class.getResource("resources/images/tracker_icon_256.png")); //$NON-NLS-1$
+		application.setDockIconImage(icon.getImage());
 	}
 	
 	/**
-	 * Constructor used by Tracker
+	 * Constructor used by Tracker, which uses the DockIcon, AboutHandler, QuitHandler
+	 * and PreferencesHandler.
+	 * 
 	 * @param app the Tracker application
 	 */
 	public OSXServices(Tracker app) {
+		tracker = app;
 		com.apple.eawt.Application application = com.apple.eawt.Application.getApplication();
-		application.setDockIconImage(Tracker.getOSXDockImage());
 		application.setAboutHandler(this);
 		application.setQuitHandler(this);
 		application.setPreferencesHandler(this);
-		tracker = app;
+		ImageIcon icon = new ImageIcon(
+	      Tracker.class.getResource("resources/images/tracker_icon_256.png")); //$NON-NLS-1$
+		application.setDockIconImage(icon.getImage());
 	}
 	
+	/**
+	 * AboutHandler
+	 */
 	public void handleAbout(com.apple.eawt.AppEvent.AboutEvent e) {
 		Tracker.showAboutTracker();
 	}
 	
+	/**
+	 * OpenFilesHandler
+	 */
 	public void openFiles(com.apple.eawt.AppEvent.OpenFilesEvent e) {
-		filesToOpen.addAll(e.getFiles());
+		List<File> files = e.getFiles();
+		String[] args = new String[files.size()];
+		for (int i=0; i<args.length; i++) {
+			args[i] = files.get(i).getAbsolutePath();
+		}
+		TrackerStarter.launchTracker(args);					 							
 	}
 	
+	/**
+	 * QuitHandler
+	 */
 	public void handleQuitRequestWith(com.apple.eawt.AppEvent.QuitEvent e,
 			com.apple.eawt.QuitResponse response) {
 		if (tracker!=null) {
@@ -66,15 +88,14 @@ public class OSXServices implements com.apple.eawt.AboutHandler,
 		response.performQuit();
 	}
 	
+	/**
+	 * PreferencesHandler
+	 */
 	public void handlePreferences(com.apple.eawt.AppEvent.PreferencesEvent e) {
 		if (tracker!=null) {
 			TFrame frame = tracker.getFrame();
 			frame.showPrefsDialog();
 		}
-	}
-
-	public ArrayList<File> getFilesToOpen() {
-		return filesToOpen;
 	}
 
 }
