ff3b1688002d0e3853420bdd410c5cab09c34929
Doug Brown <dobrown@cabrillo.edu>
2017-12-23 17:45:53 -0800

    Change copyright date to 2018

--- src/org/opensourcephysics/cabrillo/tracker/analytics/TrackerCountReader.java
+++ src/org/opensourcephysics/cabrillo/tracker/analytics/TrackerCountReader.java
@@ -314,6 +314,6 @@ public class TrackerCountReader extends JFrame {
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston MA 02111-1307 USA
  * or view the license online at http://www.gnu.org/copyleft/gpl.html
  *
- * Copyright (c) 2017  The Open Source Physics project
+ * Copyright (c) 2018  The Open Source Physics project
  *                     http://www.opensourcephysics.org
  */
