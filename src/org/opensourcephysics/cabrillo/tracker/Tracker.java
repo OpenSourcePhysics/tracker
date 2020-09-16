dbf6acf1fd5c3bf242b9511b7b1ac22503adb395
Douglas Brown <dobrown@cabrillo.edu>
2020-09-15 17:19:36 -0700

    update version to date

--- src/org/opensourcephysics/cabrillo/tracker/Tracker.java
+++ src/org/opensourcephysics/cabrillo/tracker/Tracker.java
@@ -181,7 +181,7 @@ public class Tracker implements javajs.async.SwingJSUtils.StateMachine {
 
 	// define static constants
 	/** tracker version and copyright */
-	public static final String VERSION = "5.9.20200913"; //$NON-NLS-1$
+	public static final String VERSION = "5.9.20200915"; //$NON-NLS-1$
 	public static final String COPYRIGHT = "Copyright (c) 2020 D Brown, R Hanson, W Christian"; //$NON-NLS-1$
 	
 	/**
