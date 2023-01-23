c3b55d9b7f1ef07dd43ac444cfab27522314f9ed
Doug Brown <dobrown@cabrillo.edu>
2023-01-23 14:52:02 -0800

    invalidate track data when changing reference frame

--- src/org/opensourcephysics/cabrillo/tracker/TrackChooserTView.java
+++ src/org/opensourcephysics/cabrillo/tracker/TrackChooserTView.java
@@ -530,6 +530,8 @@ public abstract class TrackChooserTView extends TView {
 					if (coords.isAdjusting())
 						return;
 				}
+				// invalidate data
+				track.invalidateData(Boolean.FALSE);
 				Step step = track.getStep(panel.getSelectedPoint(), panel);
 				view.refresh(step == null ? panel.getFrameNumber() : step.getFrameNumber(),
 						DataTable.MODE_TRACK_TRANSFORM);
