8b7fe0dab528891c44aed7280abcf20d14b1cd24
Doug Brown <dobrown@cabrillo.edu>
2024-01-16 08:27:32 -0800

    update copyright date

--- src/org/opensourcephysics/media/xuggle/XuggleMovieVideoType.java
+++ src/org/opensourcephysics/media/xuggle/XuggleMovieVideoType.java
@@ -3,7 +3,7 @@
  * frame services including implementations of the Video and VideoRecorder interfaces
  * using Xuggle (Java) and JS (JavaScript -- our minimal implementation).
  *
- * Copyright (c) 2021  Douglas Brown and Wolfgang Christian.
+ * Copyright (c) 2024  Douglas Brown and Wolfgang Christian.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -153,6 +153,6 @@ public String getTypeName() {
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston MA 02111-1307 USA
  * or view the license online at http://www.gnu.org/copyleft/gpl.html
  *
- * Copyright (c) 2021  The Open Source Physics project
+ * Copyright (c) 2024  The Open Source Physics project
  *                     https://www.compadre.org/osp
  */
--- src/org/opensourcephysics/media/xuggle/XuggleRes.java
+++ src/org/opensourcephysics/media/xuggle/XuggleRes.java
@@ -97,6 +97,6 @@ public class XuggleRes {
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston MA 02111-1307 USA
  * or view the license online at http://www.gnu.org/copyleft/gpl.html
  *
- * Copyright (c) 2021  The Open Source Physics project
+ * Copyright (c) 2024  The Open Source Physics project
  *                     http://www.opensourcephysics.org
  */
