04c68cccd2607388d6282fba11809b447fd5c3a3
Doug Brown <dobrown@cabrillo.edu>
2024-05-18 08:32:58 -0700

    eliminate debugging code

--- src/org/opensourcephysics/cabrillo/tracker/ExportZipDialog.java
+++ src/org/opensourcephysics/cabrillo/tracker/ExportZipDialog.java
@@ -845,7 +845,6 @@ public class ExportZipDialog extends JDialog implements PropertyChangeListener {
 
 	@Override
 	public void setVisible(boolean vis) {
-		System.out.println("pig start setting vis "+vis);
 		if (panelID == null)
 			return;
 		if (vis) {
@@ -853,7 +852,6 @@ public class ExportZipDialog extends JDialog implements PropertyChangeListener {
 		}
 		isVisible = vis;
 		super.setVisible(vis);
-		System.out.println("pig completed setting vis "+vis);
 	}
 
 	@Override
