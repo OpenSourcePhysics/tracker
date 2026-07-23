70be500e906129e5d6502c094a74dd226cb50e17
Doug Brown <dobrown@cabrillo.edu>
2026-07-23 16:30:08 -0700

    update Tracker count reader to read latest version(s)

--- src/org/opensourcephysics/cabrillo/tracker/analytics/TrackerCountReader.java
+++ src/org/opensourcephysics/cabrillo/tracker/analytics/TrackerCountReader.java
@@ -49,7 +49,8 @@ public class TrackerCountReader extends JFrame {
 			"clear launch log failures", "clear download failures", "test launch log", "test downloads"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	private String[] versions = {"all", "6.", "5.", "4.",  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 			"6.3", "6.2", "6.1", "6.0",   //$NON-NLS-1$//$NON-NLS-2$
-			"6.3.4", "6.3.3", "6.3.2", "6.3.1",    //$NON-NLS-1$//$NON-NLS-2$
+			"6.3.5", "6.3.4",    //$NON-NLS-1$//$NON-NLS-2$
+			"6.3.3", "6.3.2", "6.3.1",    //$NON-NLS-1$//$NON-NLS-2$
 			"6.3.0", "6.2.0", "6.1.7",    //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
 			"6.1.6", "6.1.5", "6.1.4",    //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ 
 			"6.1.3", "6.1.2", "6.1.1",    //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ 
@@ -65,9 +66,7 @@ public class TrackerCountReader extends JFrame {
 			"5.1.5", "5.1.4", "5.1.3",   //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ 
 			"5.1.2", "5.1.1", "5.1.0", "5.0.7", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 			"5.0.6", "5.0.5", "5.0.4", "5.0.3", "5.0.2", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ 
-			"5.0.1", "5.0.0", "4.11.0", "4.10.0", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-			"4.9.8", "4.97", "4.96", "4.95", "4.94",  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ 
-			"4.93", "4.92", "4.91", "4.90"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+			"5.0.1", "5.0.0"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	private String[] OSs = {"all", "windows", "osx", "linux"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	private String[] engines = {"all", "Xuggle", "none"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
 	JComboBox<String> actionDropdown, versionDropdown, osDropdown, engineDropdown;
